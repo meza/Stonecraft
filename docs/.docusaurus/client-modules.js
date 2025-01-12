80b8414dc2ac28119e07e98554149d7f99010e2f
meza <211977+meza@users.noreply.github.com>
2025-01-12 00:29:25 +0000

    docs: stonecutter example

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("F:\\dev\\Minecraft\\Mods\\minecraft-mod-gradle-plugin\\docs\\node_modules\\.pnpm\\infima@0.2.0-alpha.45\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("F:\\dev\\Minecraft\\Mods\\minecraft-mod-gradle-plugin\\docs\\node_modules\\.pnpm\\@docusaurus+theme-classic@3.7.0_@types+react@19.0.4_acorn@8.14.0_react-dom@19.0.0_react@19.0._owoxycmui6giftg53evcxcy4uy\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("F:\\dev\\Minecraft\\Mods\\minecraft-mod-gradle-plugin\\docs\\node_modules\\.pnpm\\@docusaurus+theme-classic@3.7.0_@types+react@19.0.4_acorn@8.14.0_react-dom@19.0.0_react@19.0._owoxycmui6giftg53evcxcy4uy\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("F:\\dev\\Minecraft\\Mods\\minecraft-mod-gradle-plugin\\docs\\src\\css\\custom.css"),
-];
